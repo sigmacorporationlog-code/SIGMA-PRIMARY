3681e580a5c83c4492cba5b1987de5e84c28e4f0
sigmacorporationlog-code <sigmacorporationlog@gmail.com>
2026-09-21 01:39:00 +0100

    fix: repair Windows release gate and Android compilation

--- mobile/android/app/src/main/java/com/sigma/school/MainActivity.java
+++ mobile/android/app/src/main/java/com/sigma/school/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
